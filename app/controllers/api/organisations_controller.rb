9c4ca2e14e908f15d07505f730697191a62f214d
sarahbaranski <sbaranski@gmail.com>
2022-07-03 16:09:40 -0500

    added update and delete methods to organisation

--- app/controllers/api/organisations_controller.rb
+++ app/controllers/api/organisations_controller.rb
@@ -5,11 +5,31 @@ class Api::OrganisationsController < ApplicationController
   end
 
   def create
+    user = User.find_by(id: current_user.id)
     @organisation = Organisation.new(
       name: params[:name],
       hourly_rate: params[:hourly_rate],
     )
     @organisation.save
     render "show.json.jb"
+  end
+
+  def update
+    organisation = Organisation.find_by(id: params[:id])
+    organisation.name = params[:name] || organisation.name
+    organisation.hourly_rate = params[:hourly_rate] || organisation.hourly_rate
+
+    if organisation.save
+      render json: { message: "Organisation saved." }
+    else
+      render json: { message: "Organisation did not save." }
+    end
+  end
+
+  def delete
+    user = User.find_by(id: current_user.id)
+    @organisation = Organisation.find_by(id: params[:id])
+    @organisation.destroy
+    render json: { message: "Organisation has been deleted." }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Rails.application.routes.draw do
   namespace :api do
     post "/users" => "users#create"
     post "/sessions" => "sessions#create"
+
     get "/shifts" => "shifts#index"
+
     get "/organisation" => "organisations#index"
     post "/organisation" => "organisations#create"
+    patch "/organisation/:id" => "organisations#update"
+    delete "/organisation/:id" => "organisations#delete"
   end
 end
